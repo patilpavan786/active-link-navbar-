73f2cccb6ec9094e601d61b9e8ea9b13adadfcaa
patilpavan786 <119596213+patilpavan786@users.noreply.github.com>
2023-02-21 11:48:05 +0530

    2 files modified, 1 added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import "./Navabar.css"
 export default function Navbar(){
   return(
-    <div style={{display:"flex" ,gap:"2rem"}}>
-<NavLink to ='/'>home</NavLink>
-<NavLink to='/register'>register</NavLink>
-<NavLink to='/login'>login</NavLink>
-<NavLink to='/Contactus'>Contactus</NavLink>
-<NavLink to='/Abooutus'>aboutus</NavLink>
+    <div style={{display:"flex" ,gap:"2rem", textDecoration:"none" ,backgroundColor:"aqua" , height:"3rem"}}>
+<NavLink className="navabar"  to ='/'>home</NavLink>
+<NavLink className="navabar" to='/register'>register</NavLink>
+<NavLink className="navabar" to='/login'>login</NavLink>
+<NavLink className="navabar" to='/Contactus'>Contactus</NavLink>
+<NavLink className="navabar" to='/Abooutus'>aboutus</NavLink>
     </div>
   )
 }
