1573d0f868c14e2beb942a413af1d62ec34b1e30
patilpavan786 <119596213+patilpavan786@users.noreply.github.com>
2023-03-29 20:08:26 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import Login from "./Login"
 import Register from "./Register"
 import Aboutus from "./Aboutus"
 import Contactus from "./Contactus"
+import Error from "./Error"
 import "./style.css"
 export default function App() {
   return (
     
      <Routes>
-
      <Route path='/'element={<Home/>}/>
      <Route path='/login'element={<Login />}/>
      <Route path='/register'element={<Register/>}/>
      <Route path='/Abooutus'element={<Aboutus/>}/>
      <Route path='/Contactus'element={<Contactus/>}/>
+     <Route path='*' element={<Error/>}/>
       </Routes>
     
   );
---
+++ src/Error.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Error (){
+return (
+  <>
+  <h1>error hai waps ja</h1>
+  </>
+)
+}
+
+export default Error;
